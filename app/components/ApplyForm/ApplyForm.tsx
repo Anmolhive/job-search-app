869029a355b88065e1996c9d321bc14bf83429cb
macs0416 <anmol.s@sigdesk.com>
2023-11-19 20:17:30 +0530

    form update

--- app/components/ApplyForm/ApplyForm.tsx
+++ app/components/ApplyForm/ApplyForm.tsx
@@ -81,7 +81,7 @@ const ApplyFprm = () => {
             <button onClick={handelApply} className="btn w-full glass my-5 bg-indigo-600 text-white hover:bg-indigo-950">Apply</button>
             {showForm &&
                 <div className="inset-0 overflow-y-auto sticky">
-                    <div className="w-1/2 mx-auto glass p-3 shadow-xl rounded-2xl" style={{ background: 'rgba(255,255,255,.9)' }}>
+                    <div className="w-full md:w-1/2 mx-auto glass p-3 shadow-xl rounded-2xl" style={{ background: 'rgba(255,255,255,.9)' }}>
                         <div className="w-full text-end">
                             <button onClick={() => setShowForm(false)} className="btn btn-circle btn-outline">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
